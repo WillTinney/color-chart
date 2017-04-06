117d2373bc83ae3aa0872033322ac21c5a92fdfb
Will Tinney <willtinney1@gmail.com>
2017-04-06 13:52:24 +0100

    Initialised app using create-react-app + basic formating

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Color Charts</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
