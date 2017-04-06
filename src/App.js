f68c4ad1341efacd904e6b12104b72b60afad50b
Will Tinney <willtinney1@gmail.com>
2017-04-06 17:33:12 +0100

    Palette randomizer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Palette from './Components/Palette.js';
 import './Styles/App.css';
 
 class App extends Component {
@@ -6,6 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Color Charts</h1>
+        <Palette />
       </div>
     );
   }
---
+++ src/Components/Palette.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class Palette extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      colors: ['#FFBA49', '#FF595E', '#8AC926', '#1982C4', '#6A4C93']
+    }
+  }
+
+  randomColor() {
+    return '#' + (Math.random() * 0xFFFFFF << 0).toString(16);
+  }
+
+  changePalette() {
+    let newColors = []
+    for (var i = 0; i < 6; i++) {
+      newColors.push(this.randomColor())
+    }
+    this.setState({
+      colors: newColors
+    })
+  }
+
+  handleKeyDown(e) {
+    if (e.keyCode === 32) {
+      this.changePalette();
+    }
+  }
+
+  componentWillMount() {
+    document.addEventListener("keydown", this.handleKeyDown.bind(this));
+  }
+
+  render() {
+    return (
+      <div className="palette">
+        Colors: {this.state.colors.join(', ')}
+        {this.state.colors.map(color => {
+          return <div key={color} className="palette-option" style={{backgroundColor: color}}>{color}</div>
+        })}
+      </div>
+    );
+  }
+}
+
+export default Palette;
